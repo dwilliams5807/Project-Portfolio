bd2f22c517e19208711352dfb4e1cca6f85d4c1f
Daniel Williams <daniellanierwilliams@gmail.com>
2019-10-22 22:20:11 -0500

    heroku deply

--- mail/contact_me copy.php
+++ mail/contact_me copy.php
@@ -1,22 +1,32 @@
+
 <?php
-// Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+$curl = curl_init();
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$phone = $_POST['phone']; 
+$message = $_POST['message'];
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"[dan_the_dev@icloud.com]\"\n        }\n      ],\n      \"subject\": \"New Contact\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"[noreply@danthedev.com]\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"$name<br>$email<br>$subject<br>$message\"\n    }\n  ]\n}",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer [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]",
+    "cache-control: no-cache",
+    "content-type: application/json"
+  ),
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+header('Location: thanks.html');
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-// Create the email and send the message
-$to = "dan_the_dev@icloud.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
 ?>
